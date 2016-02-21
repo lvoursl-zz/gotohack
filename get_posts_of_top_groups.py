95c4a509c5961e4de9f13596f54d0547f1c7866b
lvoursl <lvoursl@gmail.com>
2016-02-21 14:17:54 +0300

    getting last 100 vk posts for top 20 vk groups

---
+++ get_posts_of_top_groups.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from urllib.request import urlopen
+from urllib.parse import urlencode
+import json 
+
+token = '4f318b471cda5c6bd6337cfc2644b478038ee7bb09a6158395101a036b834fc315c19b3fec6b9e28ea370'
+
+groups_list = tuple(open('groups_list.txt', 'r+'))
+clear_groups_list = [ ]
+
+for group in groups_list:
+	clear_groups_list.append(int(group.replace('\n', '')))
+
+for group in clear_groups_list:
+	out_file = open('groups_posts/' + str(group), 'w')
+
+	request = urlopen('https://api.vk.com/method/wall.get?owner_id=-'+ str(group) + '&count=100&filter=owner' + '&access_token=' + token)
+	data = eval(request.read())	
+
+	for i in range(1, len(data['response'])):
+	 	if data['response'][i]['id']:
+	 		print(data['response'][i]['id'], file = out_file, end='\n')
+
+			
