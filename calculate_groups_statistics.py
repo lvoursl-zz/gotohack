5781b7ba0132860095cc068aaaa8230e5504c516
lvoursl <lvoursl@gmail.com>
2016-02-21 19:28:41 +0300

    add prototype of like counter

--- calculate_groups_statistics.py
+++ calculate_groups_statistics.py
@@ -1,3 +1,7 @@
+from urllib.request import urlopen
+
+token = '4f318b471cda5c6bd6337cfc2644b478038ee7bb09a6158395101a036b834fc315c19b3fec6b9e28ea370'
+
 groups_file = tuple(open('groups_list.txt', 'r+'))
 groups_list = [ ]
 
@@ -17,10 +21,15 @@ for group in groups_list:
 	file_of_posts_likes = open('calculated_likes_for_posts/' + str(group), 'w+')
 	
 	# для VK API : type = post; owner_id = group; item_id = post; 
-	for post in posts_list
-		# считаем количество лайков для поста
+	for post in posts_list:
 		post_likes = 0
-		print(post_likes, file = file_of_posts_likes, end = '\n')
+		# считаем количество лайков для поста		
+		for user_id in users_ids:
+			request = urlopen('https://api.vk.com/method/likes.isLiked?user_id=' + str(user_id) + '&' + 'type=post&owner_id=' + str(group) + '&item_id=' + str(post) + '&access_token=' + token)
+			data = eval(request.read())	
+			# if data ne empty.. to post_likes += 1
+
+		print(post_likes, file = file_of_posts_likes, end = '\n')		
 	
 	file_of_posts_likes.close()
 
